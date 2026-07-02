b4b0cd9453d9f25f9b603b56da66aed96e164ff6
a95182807-bit <a95182807@gmail.com>
2026-07-02 21:07:18 +0600

    Update Mini App

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function hello(){
-    alert("Welcome to Telegram Mini App");
+function hello() {
+    document.body.innerHTML = 
+        <h1>🎉 Welcome Rony!</h1>
+        <p>Your Telegram Mini App is working successfully.</p>
+    ;
 }
