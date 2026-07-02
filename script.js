eac442c192bdc167374eb7c79090e8f81a68dc45
a95182807-bit <a95182807@gmail.com>
2026-07-02 21:47:12 +0600

    Update Mini App

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function startTask() {
-    document.body.innerHTML =
-        "<h1>🎉 Welcome Rony</h1>" +
-        "<p>Your Telegram Mini App is working successfully.</p>";
-}
-
-function withdraw() {
-    alert("Withdraw feature is coming soon!");
-}
+alert("script.js loaded");
