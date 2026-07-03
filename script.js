b77d29b039f34adacabbc8cd880331e166be0722
a95182807-bit <a95182807@gmail.com>
2026-07-03 20:57:02 +0600

    Add Monetag Poupup

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 Telegram.WebApp.ready();
 Telegram.WebApp.expand();
+
 document.getElementById("startBtn").addEventListener("click", function () {
-    alert("Start Task clicked!");
+
+    show_11235103('pop').then(() => {
+        alert("🎉 Congratulations! You watched the ad.");
+    }).catch(() => {
+        alert("Ad was not completed.");
+    });
+
 });
 
 document.getElementById("withdrawBtn").addEventListener("click", function () {
